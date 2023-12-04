5cee1d78406ca25237d0f8fc6b0dba1f42e55247
Alexandru Botas <43759266+alxandruu@users.noreply.github.com>
2023-12-04 19:08:55 +0100

    style(admin-panels): improve css and mozilla compatibility

--- src/app/components/admin-panel/resources-ap.component.ts
+++ src/app/components/admin-panel/resources-ap.component.ts
@@ -5,7 +5,7 @@ import { Category } from 'src/app/models/interfaces';
   selector: 'ap-resources',
   template: `
     <admin-action-new-resource [categories]=categories></admin-action-new-resource>
-    <admin-action-edit-resource [categories]=categories style="position:relative; top:3.5rem"></admin-action-edit-resource>
+    <admin-action-edit-resource [categories]=categories></admin-action-edit-resource>
   `
 })
 
